77594efd396675c355fe851f980b2de1f89f92ce
Enrique Finetti <enriquefinetti@gmail.com>
2023-03-02 14:44:19 -0300

    Update colores.js

--- colores.js
+++ colores.js
@@ -1,24 +1,23 @@
-document.getElementById('personal').onmouseover=
+document.getElementById("box").addEventListener("mouseover", function() {
+    document.getElementById("box").style.backgroundColor = "rgb(10, 160, 140)";
+});
+    
+document.getElementById("box").addEventListener("mouseout", function() {
+    document.getElementById("box").style.backgroundColor = "rgb(85, 185, 130)";
+});
 
-var container = $('#container');
+document.getElementById("box2").addEventListener("mouseover", function() {
+    document.getElementById("box2").style.backgroundColor = "rgb(10, 160, 140)";
+});
+    
+document.getElementById("box2").addEventListener("mouseout", function() {
+    document.getElementById("box2").style.backgroundColor = "rgb(85, 185, 130)";
+});
 
-$('.codepen').hover(function(){
-   container.css({backgroundColor: "#000000"});
-}, function(){
-   container.css({backgroundColor: "#eee"});
+document.getElementById("box3").addEventListener("mouseover", function() {
+    document.getElementById("box3").style.backgroundColor = "rgb(10, 160, 140)";
 });
-$('.github').hover(function(){
-   container.css({backgroundColor: "#4078c0"});
-}, function(){
-   container.css({backgroundColor: "#eee"});
-});
-$('.dropbox').hover(function(){
-   container.css({backgroundColor: "#1081de"});
-}, function(){
-   container.css({backgroundColor: "#eee"});
-});
-$('.bitbucket').hover(function(){
-   container.css({backgroundColor: "#205081"});
-}, function(){
-   container.css({backgroundColor: "#eee"});
+    
+document.getElementById("box3").addEventListener("mouseout", function() {
+    document.getElementById("box3").style.backgroundColor = "rgb(85, 185, 130)";
 });
