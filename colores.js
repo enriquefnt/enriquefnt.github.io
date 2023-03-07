7c72dd3bb6be180fad6a06f6ecadf6e261a81e07
enriquefnt <enriquefinetti@gmail.com>
2023-03-07 11:59:42 -0300

    arreglos finales

--- colores.js
+++ colores.js
@@ -21,3 +21,9 @@ document.getElementById("box3").addEventListener("mouseover", function() {
 document.getElementById("box3").addEventListener("mouseout", function() {
     document.getElementById("box3").style.backgroundColor = "rgb(85, 185, 130)";
 });
+
+
+var emailLink = document.getElementById('emailEnlace');
+		var emailAddress = 'ernestof@gmail.com';
+
+		emailLink.setAttribute('href', 'mailto:' + emailAddress);
